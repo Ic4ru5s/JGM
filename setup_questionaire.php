4bbc81b927783f8411969734751a21c224b6d330
Ard van Noordenne <ard@noordpost.nl>
2022-11-26 11:42:52 +0100

    Version 5: Works with quirks

--- setup_questionaire.php
+++ setup_questionaire.php
@@ -11,21 +11,41 @@
         <form action="setup_questionaire.php" method="post"> <!-- Collect the data required to make question cards -->
             Question: <input type="text" name="question"><br>
             Tag: <input type="text" name="tag"><br>
-            Answer type: <select name="answertype" id="answertype">
-                <option value="radio">Radio button</option>
+            Answer type: <select name="answertype" id="answertype" onChange="unlockOption(this)">
                 <option value="text">Tekstvak</option>
+                <option value="radio">Radio button</option>
                 <option value="date">Datum</option>
                 <option value="integer">Getal</option>
                 <option value="check">Checkbox</option>
             </select><br>
-            Options: <input type="text" name="option1"><br> <!-- Try to add more options with a button -->
-            <input type="text" name="option2"><br>
-            <input type="text" name="option3"><br>
-            <input type="text" name="option4"><br>
-            <input type="text" name="option5"><br>
-            <input type="text" name="option6"><br>
+            <div id="optionlist"></div>
+            <input type="hidden" id="optioncount" name="optioncount">
             <input type="submit">
         </form>
+
+        <script>
+            const optionlist = document.getElementById("optionlist");
+            let optioncounter = 0
+
+            function unlockOption(optionvalue) {
+                let option = optionvalue.value;
+
+                if (option === "radio" || option === "check") { //determine answertype
+                    const element = document.createElement('div');
+                    element.innerHTML = "Options:" + '<input type="text" name="option0">' + '<button type="button" onclick="addOption()">+</button>'; // adds the first line option input
+                    optionlist.appendChild(element);
+                    document.getElementById("optioncount").value = optioncounter
+                }
+            }
+
+            function addOption() { // creates more input elements
+                optioncounter += 1
+                const element = document.createElement('div');
+                element.innerHTML ='<p>' + optioncounter + '<input type="text" name="option' + optioncounter + '">';
+                optionlist.appendChild(element);
+                document.getElementById("optioncount").value = optioncounter
+            }
+        </script>
 
         <?php
         ini_set('display_errors', 1);
@@ -44,13 +64,27 @@
         die("Connection failed: " . $conn->connect_error);
         }
 
-        if ($_POST["question"] != NULL || $_POST["tag"] != NULL || $_POST["answertype"] != NULL) { // prevents empty rows in table
-                $sql = "INSERT INTO questions (question, tag, answertype) VALUES ('$_POST[question]', '$_POST[tag]', '$_POST[answertype]')";
+        $currentoption = 0;
+        $optionlist = "";       
+
+        while ($currentoption <= $_POST["optioncount"]) {
+            $name = "option" . $currentoption;
+
+            if ($_POST[$name] != "") {
+                $optionlist = $optionlist . $_POST[$name] . "|";
+            } 
+            
+            $currentoption++;
+        } 
+
+
+        if ($_POST["question"] != "" || $_POST["tag"] != "" || $_POST["answertype"] != "") { // prevents empty rows in table
+                $sql = "INSERT INTO questions (question, tag, answertype, options) VALUES ('$_POST[question]', '$_POST[tag]', '$_POST[answertype]', '$optionlist')";
 
             if ($conn->query($sql) === TRUE) {
-                echo "question creation succesful";
+                //echo "Question creation succesful";
                 } else {
-                echo "Error creating table: " . $conn->error;
+                echo "Error creating question: " . $conn->error;
                 }
         }
         ?>
@@ -58,14 +92,23 @@
         <br><button class="btn line_button_black float_right " style="margin-top:15px;" onclick="window.location.href='index.php'">Home</button>
     </div>
 
-    <div> <!-- Question display panel -->
+    <div> <!-- Question display panel --> 
         <?php
-            $sql = "SELECT id, question, tag FROM questions";
+            $sql = "SELECT id, question, tag, options FROM questions";
             $result = $conn->query($sql);
             
             if ($result->num_rows > 0) {
                 // output data of each row
                 while($row = $result->fetch_assoc()) {
+                    if ($row["options"] != NULL) {
+                        $options = explode("|", $row["options"]);
+                        $counter = 0;
+                        while ($counter < count($options)) {
+                            echo $options[$counter] . "<br>";
+                            $counter++;
+                        }
+                    }
+
                     echo "<br> id: ". $row["id"]. " - Name: ". $row["question"]. " " . $row["tag"] . "<br>";
                 }
             } else {
